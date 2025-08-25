fc34f6841dc7b33a792c98289425e07416cfc167
Rahiman13 <rahimanshaik13@gmail.com>
2025-08-25 12:48:07 +0530

    offer temp4

--- utils/offerLetterTemplate.js
+++ utils/offerLetterTemplate.js
@@ -27,7 +27,7 @@ module.exports = (user) => {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Offer Letter</title>
-    <style>
+    <style> 
         @page {
             size: A4;
             margin: 0;
